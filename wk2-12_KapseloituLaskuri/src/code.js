2176f4801470560b5474cd7f6bda2639dd55a6b6
Icaros <miska@ronin.fi>
2017-07-19 10:31:30 +0300

    KapseloituLaskuri

---
+++ wk2-12_KapseloituLaskuri/src/code.js
@@ -0,0 +1,46 @@
+// function getFunc() {
+//   var luku = 0;
+//
+//   function kasvata() {
+//     luku++;
+//   }
+//
+//   function annaLuku() {
+//     return luku;
+//   }
+//
+//   return {
+//     kasvata: kasvata,
+//     annaLuku: annaLuku
+//   };
+// }
+//
+// var laskin = getFunc();
+
+var laskin = (function() {
+  var luku = 0;
+
+  function kasvata() {
+    luku++;
+  }
+
+  function annaLuku() {
+    return luku;
+  }
+
+  return {
+    kasvata: kasvata,
+    annaLuku: annaLuku
+  };
+})();
+
+
+function kasvataJaAseta() {
+  laskin.kasvata();
+  document.getElementById("laskuri").innerHTML = laskin.annaLuku();
+}
+
+function init() {
+  var nappi = document.getElementById("nappi");
+  nappi.addEventListener("click", kasvataJaAseta, false);
+}
